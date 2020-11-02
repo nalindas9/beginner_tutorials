4cd304739d961a7485838d55f8a511aaff44f5c7
Nalin Das <nalindas9@gmail.com>
2020-11-02 13:08:29 -0500

    Add doxygen comments
    [beginner_tutorials/src/listener.cpp] Add comments
    
    [beginner_tutorials/src/talker.cpp] Add comments

--- beginner_tutorials/src/listener.cpp
+++ beginner_tutorials/src/listener.cpp
@@ -1,63 +1,39 @@
-/*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
+/**
+ *  Copyright 2020 Nalin Das
+ *  @file listener.cpp
+ *  @author Nalin Das
+ *  @date 11/02/2020
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
+ *  @brief ROS client 
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ *  @section DESCRIPTION
+ *
+ *  Source file for ros client to add two integers
+ *
  */
 
-// %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "beginner_tutorials/AddTwoInts.h"
 
 /**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-// %EndTag(CALLBACK)%
-
+ * @brief Main function
+ * @param argc Command line number of arguments
+ * @param argv Command line arguments vector
+ * @return 0
+ * **/
 int main(int argc, char **argv) {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+
+  // Initialize ros node
   ros::init(argc, argv, "listener");
 
+  // Check length of argument vector
   if (argc != 3) {
     ROS_INFO_STREAM("Usage: add_two_ints_client X Y");
     return 1;
   }
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
+  // Create ROS nodehandle object
   ros::NodeHandle n;
 
   // Service client
@@ -65,6 +41,7 @@ int main(int argc, char **argv) {
   beginner_tutorials::AddTwoInts srv;
   srv.request.a = atoll(argv[1]);
   srv.request.b = atoll(argv[2]);
+  // If call to service successfull then displace sum
   if (client.call(srv)) {
     ROS_INFO_STREAM("Sum: " << (long int)srv.response.sum);
   } else {
@@ -75,4 +52,3 @@ int main(int argc, char **argv) {
 
   return 0;
 }
-// %EndTag(FULLTEXT)%
